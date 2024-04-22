8a80f3e73443d03ded9e6250c25f7d87b033e43b
Bedyusha <93040804+Bedyusha@users.noreply.github.com>
2024-04-22 16:59:32 +0300

    Выбор фото теперь работает

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import json
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRaisedButton
@@ -10,29 +12,52 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
+from kivy.uix.behaviors import ButtonBehavior
 
+class ImageButton(ButtonBehavior, Image):
+    pass
 class PetProfileScreen(Screen):
     def __init__(self, **kwargs):
         super(PetProfileScreen, self).__init__(**kwargs)
         self.name = 'pet_profile'
         self.layout = BoxLayout(orientation='horizontal')
-        self.image = Image(source='default_pet_image.jpg', size_hint=(0.5, 1))
+        # Загрузить сохраненный путь к изображению, если он существует
+        self.image_path = self.load_image_path()
+        self.image = ImageButton(source=self.image_path, size_hint=(0.5, 1))
         self.image.bind(on_release=self.open_file_chooser)
         self.layout.add_widget(self.image)
         self.text_input = TextInput(hint_text='Введите имя питомца', size_hint=(0.5, 1))
         self.layout.add_widget(self.text_input)
-        self.file_chooser = FileChooserIconView(size_hint=(1, 0.4))
         self.add_widget(self.layout)
 
     def open_file_chooser(self, *args):
-        popup = Popup(title='Выберите изображение', content=self.file_chooser, size_hint=(0.9, 0.9))
+        self.file_chooser = FileChooserIconView(size_hint=(1, 0.4))
         self.file_chooser.bind(on_submit=self.load_image)
-        popup.open()
+        self.popup = Popup(title='Выберите изображение', content=self.file_chooser, size_hint=(0.9, 0.9))
+        self.popup.open()
 
     def load_image(self, instance, value, touch):
-        self.image.source = value[0]
-        instance.unbind(on_submit=self.load_image)
+        if value:
+            self.image.source = value[0]
+            # Сохранить путь к изображению
+            self.save_image_path(value[0])
+            instance.unbind(on_submit=self.load_image)
+            self.popup.dismiss(force=True)
 
+    def save_image_path(self, path):
+        # Сохранить путь к изображению в файле
+        with open('image_path.json', 'w') as f:
+            json.dump({'image_path': path}, f)
+
+    def load_image_path(self):
+        # Загрузить путь к изображению из файла, если он существует
+        if os.path.exists('image_path.json'):
+            with open('image_path.json', 'r') as f:
+                data = json.load(f)
+                return data['image_path']
+        # Вернуть путь к изображению по умолчанию, если файл не существует
+        return 'cat-defolt.png'
+    
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
