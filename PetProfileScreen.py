5c7007c4de02f31a5c6e1ede56b5f9ac3f99a467
Bedyusha <93040804+Bedyusha@users.noreply.github.com>
2024-05-31 00:27:36 +0300

    начало добавления питания

--- PetProfileScreen.py
+++ PetProfileScreen.py
@@ -12,9 +12,6 @@ from kivymd.uix.textfield import MDTextField
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.pickers import MDDatePicker
 from kivy.uix.spinner import Spinner
-from kivymd.uix.dropdownitem import MDDropDownItem
-from kivymd.uix.menu import MDDropdownMenu
-
 
 class ImageButton(ButtonBehavior, Image):
     pass
@@ -29,9 +26,6 @@ class PetProfileScreen(Screen):
         # Загрузить сохраненный путь к изображению, если он существует
         self.image_path = self.load_image_path()
         self.image = ImageButton(source=self.image_path, size_hint=(None, None), size=(200, 200), pos_hint={'center_x': 0.5})
-        # Установить минимальный и максимальный размер изображения
-        self.image.size_hint_min = (100, 100)  # Минимальный размер 100x100 пикселей
-        self.image.size_hint_max = (300, 300)  # Максимальный размер 500x500 пикселей
         self.image.bind(on_release=self.open_file_chooser)
         self.layout.add_widget(self.image)
 
@@ -55,20 +49,15 @@ class PetProfileScreen(Screen):
         self.pet_info = self.load_pet_info()
 
         # Создать метки и текстовые поля для ввода информации о питомце
+
         self.text_inputs = {}
         self.text_inputs['pet_name'] = self.create_textinput('Кличка питомца:', 'pet_name')
         self.text_inputs['pet_breed'] = self.create_textinput('Порода:', 'pet_breed')
         self.text_inputs['owner_name'] = self.create_textinput('Хозяин:', 'owner_name')
+        self.text_inputs['pet_birthday'] = self.create_date_input('Дата рождения:', 'pet_birthday')
 
-        # Добавить поля для ввода даты рождения
-        self.create_label_and_textinput('Дата рождения:', 'pet_birthday')
-
-        # Загрузить и отобразить сохраненную дату рождения
         if 'birth_date' in self.pet_info:
-            year, month, day = self.pet_info['birth_date'].split('-')
-            self.text_inputs['day'].text = day
-            self.text_inputs['month'].text = month
-            self.text_inputs['year'].text = year
+            self.text_inputs['pet_birthday'].text = self.pet_info['birth_date']
 
         self.add_widget(self.layout)
 
@@ -79,38 +68,11 @@ class PetProfileScreen(Screen):
         with open('pet_info.json', 'w') as f:
             json.dump(self.pet_info, f)
 
-    def save_date(self, instance, the_date, the_time):
-        self.pet_info['birth_date'] = str(the_date)
-        with open('pet_info.json', 'w') as json_file:
-            json.dump(self.pet_info, json_file)
-        # Обновить текстовые поля после сохранения даты
-        year, month, day = str(the_date).split('-')
-        self.text_inputs['day'].text = day
-        self.text_inputs['month'].text = month
-        self.text_inputs['year'].text = year
-
-    def create_label_and_textinput(self, label_text, info_key):
-        # Создать текстовые поля для ввода даты
-        if info_key == 'pet_birthday':
-            date_layout = BoxLayout(orientation='horizontal', size_hint=(1, None), height=35)
-            self.text_inputs['day'] = self.create_date_input('День', 'day')
-            self.text_inputs['month'] = self.create_date_input('Месяц', 'month')
-            self.text_inputs['year'] = self.create_date_input('Год', 'year')
-            date_layout.add_widget(self.text_inputs['day'])
-            date_layout.add_widget(self.text_inputs['month'])
-            date_layout.add_widget(self.text_inputs['year'])
-            self.info_layout.add_widget(date_layout)
-            return date_layout  # Возвращаем layout с полями для ввода даты
-        else:
-            text_input = MDTextField(text=self.pet_info.get(info_key, ''), hint_text=label_text, size_hint=(1, None), font_size='15sp', mode="rectangle", line_color_normal=get_color_from_hex("#2C2C2C"), line_color_focus=get_color_from_hex("#2C2C2C"))
-            text_input.bind(text=self.save_pet_info(info_key))  # Привязать функцию save_pet_info к событию изменения текста
-            self.info_layout.add_widget(text_input)
-            return text_input
-
     def create_date_input(self, hint_text, info_key):
         # Создать текстовое поле для ввода даты
-        text_input = MDTextField(text=self.pet_info.get(info_key, ''), hint_text=hint_text, size_hint=(None, None), width=60, font_size='15sp', mode="rectangle", line_color_normal=get_color_from_hex("#2C2C2C"), line_color_focus=get_color_from_hex("#2C2C2C"))
+        text_input = MDTextField(text=self.pet_info.get(info_key, ''), hint_text=hint_text, size_hint=(1, None), font_size='15sp', mode="rectangle", line_color_normal=get_color_from_hex("#2C2C2C"), line_color_focus=get_color_from_hex("#2C2C2C"))
         text_input.bind(on_touch_down=self.show_date_picker)  # Привязать функцию show_date_picker к событию изменения фокуса
+        self.info_layout.add_widget(text_input)
         return text_input
 
     def show_date_picker(self, instance, touch):
@@ -119,6 +81,12 @@ class PetProfileScreen(Screen):
             date_picker.bind(on_save=self.save_date)
             date_picker.open()
 
+    def save_date(self, instance, the_date, the_time):
+        self.pet_info['birth_date'] = str(the_date)
+        with open('pet_info.json', 'w') as json_file:
+            json.dump(self.pet_info, json_file)
+        # Обновить текстовые поля после сохранения даты
+        self.text_inputs['pet_birthday'].text = str(the_date)
 
     def validate_and_save_text(self, info_key):
         def save_text(instance, value):
@@ -142,15 +110,6 @@ class PetProfileScreen(Screen):
         text_input.bind(text=self.validate_and_save_text(info_key))  # Привязать функцию validate_and_save_text к событию изменения текста
         self.info_layout.add_widget(text_input)
         return text_input
-
-    def save_pet_info(self, info_key):
-        def save_text(instance, value):
-            # Сохранить значение текстового поля для ввода в информацию о питомце
-            self.pet_info[info_key] = value
-            # Сохранить информацию о питомце в файле
-            with open('pet_info.json', 'w') as f:
-                json.dump(self.pet_info, f)
-        return save_text
 
     def load_pet_info(self):
         # Загрузить информацию о питомце из файла, если он существует
